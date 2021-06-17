463cf980e784a79bfd66f4db0aeed3a8bd343d4d
md-mostafa <md.mostafa.akash@gmail.com>
2021-06-17 18:44:10 +0600

    create post

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -17,3 +17,14 @@ export const getPosts = () => async(dispatch) => {            //this is redux th
     //return action;
     //dispatch(action);
 }
+
+
+export const createPost = (post) => async (dispatch) => {
+    try{
+        const { data } = await api.createPost(post);
+
+        dispatch({ type: 'CREATE', payload: data });
+    }catch (error) {
+        console.log(error.message);
+    }
+}
