40d44da309f5315dfa9bc26482d49ed204d7a817
md-mostafa <md.mostafa.akash@gmail.com>
2021-07-01 00:37:33 +0600

    like functionality

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -7,7 +7,7 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
 
-import { deletePost } from '../../../actions/posts';
+import { deletePost, likePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
@@ -37,10 +37,9 @@ const Post = ({ post, setCurrentId }) => {
                 <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
             
-            <Button size="small" color="primary" onClick={() =>{}}>
+            <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
                 <ThumbUpAltIcon fontSize="small" />
-                Like
-                {post.likeCount}
+                Like {post.likeCount}
             </Button>
             <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
                 <DeleteIcon fontSize="small" />
