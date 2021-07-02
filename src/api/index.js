d75070dd53972f0b3f7907dbb286b4b8fab23642
md-mostafa <md.mostafa.akash@gmail.com>
2021-07-02 20:03:42 +0600

    like edit delete

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,14 @@ const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 //const url = 'https://memories-ms.herokuapp.com/posts';
 
+API.interceptors.request.use((req) => { //this is for auth middleware //specific for every request //need to send the token to backend so that our backend middleware verify user is loggedin
+    if(localStorage.getItem('profile')) {
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+
+    return req;
+});
+
 
 export const fetchPosts = () => API.get('/posts');
 export const createPost = (newPost) => API.post('/posts', newPost);
