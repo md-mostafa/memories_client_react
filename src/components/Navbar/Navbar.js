91ac492d05dbe66481ef806a0e712f24ed32013a
md-mostafa <md.mostafa.akash@gmail.com>
2021-07-02 20:16:46 +0600

    token expiry

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import { AppBar, Typography , Avatar, Toolbar, Button } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
+import decode from 'jwt-decode';
 
 
 import useStyles from './styles';
@@ -28,6 +29,12 @@ const Navbar = () => {
 
 
         //JWT ...
+        //check if token is expired or not
+        if(token) {
+            const decodedToken = decode(token);
+
+            if(decodedToken.exp * 1000 < new Date().getTime()) logout(); //calling logout action
+        }
 
         setUser(JSON.parse(localStorage.getItem('profile')));
     }, [location]);
