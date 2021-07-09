8849b8978e25cb6a8199631bfcfeaaac041b5d5b
md-mostafa <md.mostafa.akash@gmail.com>
2021-07-09 20:44:17 +0600

    basic pagination and search route

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid, Paper } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
-
 import { getPosts } from '../../actions/posts';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
+import Pagination from '../Pagination';
+
+
 
 const Home = () => {
   const [currentId, setCurrentId] = useState(0);
@@ -23,6 +25,9 @@ const Home = () => {
           </Grid>
           <Grid item xs={12} sm={12} md={4}>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
+            <Paper elevation={6}>
+              <Pagination />
+            </Paper>
           </Grid>
         </Grid>
       </Container>
@@ -31,44 +36,3 @@ const Home = () => {
 };
 
 export default Home;
-
-/*
-
-
-import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-
-import Posts from '../Posts/Posts';
-import Form from '../Form/Form';
-import { getPosts } from '../../actions/posts';
-
-
-const Home = () => {
-    const [currentId, setCurrentId] = useState(null);
-    const dispatch = useDispatch(); //Its a hook    
-
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
-
-    return (
-        <Grow in>
-            <Container>
-                <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                    <Grid item xs={12} sm={12} md={7}>
-                        <Posts setCurrentId = {setCurrentId} />
-                    </Grid>
-                    <Grid item xs={12} sm={12} md={4}>
-                        <Form currentId = {currentId} setCurrentId = {setCurrentId} />
-                    </Grid>
-                </Grid>
-            </Container>
-        </Grow>
-    )
-};
-
-export default Home;
-
-
-*/
